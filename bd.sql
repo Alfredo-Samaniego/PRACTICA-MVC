fc855b7fcefdece0c539a5f5f09eba1835bdef8f
DESKTOP-I75LOC7\User <fsama600@gmail.com>
2020-10-26 00:18:22 -0600

    Practica MVC

---
+++ bd.sql
@@ -0,0 +1,17 @@
+
+CREATE DATABASE IF NOT EXISTS `colegio` 
+USE `colegio`;
+
+
+-- Volcando estructura para tabla colegio.alumnos
+CREATE TABLE IF NOT EXISTS `alumnos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(50) NOT NULL DEFAULT '0',
+  `Apellido` varchar(50) NOT NULL DEFAULT '0',
+  `Sexo` tinyint(4) NOT NULL DEFAULT '0',
+  `FechaNacimiento` varchar(20) DEFAULT NULL,
+  `FechaRegistro` varchar(20) DEFAULT NULL,
+  `Correo` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
